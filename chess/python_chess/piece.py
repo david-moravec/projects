0b1396eb796e6d3732cb9176674576064daee464
David Moravec <david.moravec@cfdsupport.com>
2021-08-17 15:18:30 +0200

    vytvoreni classy Piece

--- chess/python_chess/piece.py
+++ chess/python_chess/piece.py
@@ -1,17 +1,30 @@
-class Knight:
+class Piece:
     def __init__(self, x, y, team, board):
         self.team  = team
-        self.move(x, y, board)
+        self.movePiece(x, y, board)
+
+    def movePiece(self, new_x, new_y, board):
+        self.makeMove(new_x, new_y)
+        self.updateBoard(board)
+
+    def makeMove(self, x, y):
+        if self.checkMove(x, y) == True:
+            self.x = x
+            self.y = y
+
+    def updateBoard(self, board):
+        board.update(self)
+
+    def checkMove(self, x, y):
+        pass
+
+class Knight(Piece):
+    def __init__(self, x, y, team, board):
+        Piece.__init__(self, x, y, team, board)
 
     def __str__(self):
         return "k"
 
-    def move(self, new_x, new_y, board):
-        if self.checkMove() == True:
-            self.x = new_x
-            self.y = new_y
-            board.update(self)
-
-    def checkMove(self):
+    def checkMove(self, x, y):
         return True
         
