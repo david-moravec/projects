e347901eeecd3a6329355371eec934bf99edb175
David Moravec <david.moravec@cfdsupport.com>
2022-04-28 10:48:01 +0200

    skoro finish, potreba upravit losovani cinnosti, ted to nebude kazdemu davat jinou cinnost

--- mailing-services/mailingManager.py
+++ mailing-services/mailingManager.py
@@ -1,25 +1,16 @@
 import smtplib
-from message import message
-form top_secret import sender_address, sender_pass, receiver_address
 from email.mime.multipart import MIMEMultipart
 
-class Mailing:
-    def __init__(self, sender: str, sender_pass: str, recievers: list, message: MIMEMultipart):
+class MailingManager:
+    def __init__(self, sender: str, sender_pass: str, recipients: dict):
         self.sender = sender
         self.sender_pass = sender_pass
-        self.recievers = recievers
-        self.message = message
+        self.addresses_to = recipients
 
-    def sendMassage(self):
+    def sendMassage(self, reciever: str, message):
         session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
         session.starttls() #enable security
         session.login(self.sender, self.sender_pass) #login with mail_id and password
         text = message.as_string()
-        session.sendmail(sender_address, receiver_address, text)
+        session.sendmail(self.sender, reciever, text)
         session.quit()
-
-
-
-#teslaMaxCyclone = Mailing(sender_address, sender_pass, [receiver_address], message)
-#teslaMaxCyclone.sendMassage()
-#print('Mail Sent')
--- mailing-services/serviceManager.py
+++ mailing-services/serviceManager.py
@@ -1,17 +1,48 @@
 import pandas as pd
 from datetime import date, datetime, timedelta
-from mailingManager import Mailing
+from mailingManager import MailingManager
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-class ServiceManager:
-    def __init__(self, service_file: str): 
-        self.services = pd.read_csv(service_file)
-        print(self.services.iloc[self.getWeekNumber() + 1])
+from top_secret import sender_address, app_pass, roommates
+
+class ServiceManager(MailingManager):
+    def __init__(self, sender: str, app_pass: str, recipients: dict): 
+        MailingManager.__init__(self, sender, app_pass, roommates)
+        self.services = ("Kuchyn + setrit", "Koupelny", "Luxovani")
         self.getDates()
-
+        for key, date in self.dates.items():
+            print(key, date)
+        self.createAndSendMessageBody()
 
     @staticmethod
     def getWeekNumber():
         return date.today().isocalendar()[1]
+
+    def createAndSendMessageBody(self):
+        self.assignChores()
+        for key, chore in self.chores_assigned.items():
+            message = MIMEMultipart()
+            message['From'] = "{} <{}>".format("Sluzby Byt", self.sender)
+            message['To'] = self.addresses_to[key]
+            message['Subject'] = 'Sluzby {}. tyden'.format(self.getWeekNumber()) #The subject line
+            mail_content = """Pic,
+            Tenhle tyden ({} - {}) te ceka 
+            {}
+            Dik""".format(self.dates['monday'].strftime("%d. %m."), self.dates['sunday'].strftime("%d. %m."), chore)
+            message.attach(MIMEText(mail_content, 'plain'))
+            try:
+                self.sendMassage(self.addresses_to[key], message)
+                print("Message sent!")
+            except Exception as e:
+                print(e)
+
+    def assignChores(self):
+        self.chores_assigned = {}
+        week_no = self.getWeekNumber()
+        for key in self.addresses_to.keys():
+            self.chores_assigned[key] = self.services[week_no % 3]
+
 
     def getDates(self):
         self.dates = {}
@@ -22,4 +53,5 @@ class ServiceManager:
         self.dates['monday'] = today + timedelta(days=delta_to_next_monday)
         self.dates['sunday'] = today + timedelta(days=delta_to_next_monday + days_in_week - 2)
 
-ServiceManager("./sluzby.csv")
+if __name__ == "__main__":
+    ServiceManager(sender_address, app_pass, roommates)
