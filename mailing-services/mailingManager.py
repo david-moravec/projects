3913fefc27fa9e9b50ce9c9989a4cc4fd3f1a513
David Moravec <david.moravec@cfdsupport.com>
2022-04-28 13:58:38 +0200

    dela, co ma

--- mailing-services/mailingManager.py
+++ mailing-services/mailingManager.py
@@ -7,11 +7,16 @@ class MailingManager:
         self.sender_pass = sender_pass
         self.addresses_to = recipients
 
+    def startSession(self):
+        print("Starting session\n\n")
+        self.session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+        self.session.starttls() #enable security
+        self.session.login(self.sender, self.sender_pass) #login with mail_id and password
+
+    def endSession(self):
+        print("\n\nEnding mailing session")
+        self.session.quit()
+
     def sendMassage(self, reciever: str, message):
-        session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
-        session.starttls() #enable security
-        session.login(self.sender, self.sender_pass) #login with mail_id and password
         text = message.as_string()
-        session.sendmail(self.sender, reciever, text)
-        session.quit()
-        session.sendmail(sender_address, receiver_address, text)
+        self.session.sendmail(self.sender, reciever, text)
--- mailing-services/serviceManager.py
+++ mailing-services/serviceManager.py
@@ -7,9 +7,10 @@ from email.mime.text import MIMEText
 from top_secret import sender_address, app_pass, roommates
 
 class ServiceManager(MailingManager):
+    roomamtes = 3
     def __init__(self, sender: str, app_pass: str, recipients: dict): 
         MailingManager.__init__(self, sender, app_pass, roommates)
-        self.services = ("Kuchyn + setrit", "Koupelny", "Luxovani")
+        self.services = ("uklidit kuchyn + vytrit", "uklidit koupelny", "vyluxovat")
         self.getDates()
         for key, date in self.dates.items():
             print(key, date)
@@ -21,27 +22,31 @@ class ServiceManager(MailingManager):
 
     def createAndSendMessageBody(self):
         self.assignChores()
+        self.startSession()
         for key, chore in self.chores_assigned.items():
             message = MIMEMultipart()
             message['From'] = "{} <{}>".format("Sluzby Byt", self.sender)
             message['To'] = self.addresses_to[key]
             message['Subject'] = 'Sluzby {}. tyden'.format(self.getWeekNumber()) #The subject line
-            mail_content = """Pic,
-            Tenhle tyden ({} - {}) te ceka 
-            {}
-            Dik""".format(self.dates['monday'].strftime("%d. %m."), self.dates['sunday'].strftime("%d. %m."), chore)
+            mail_content = "Pic {},\n    Tenhle tyden ({} - {}) je potreba:\n    {}\nDik\nNavzdy vas\nTeslaCycloneMax :*".format(key, self.dates['monday'].strftime("%d. %m."), self.dates['sunday'].strftime("%d. %m."), chore)
             message.attach(MIMEText(mail_content, 'plain'))
+            print(message.as_string())
             try:
-                self.sendMassage(self.addresses_to[key], message)
+                #self.sendMassage(self.addresses_to[key], message)
                 print("Message sent!")
             except Exception as e:
                 print(e)
 
+        self.endSession()
+
     def assignChores(self):
         self.chores_assigned = {}
         week_no = self.getWeekNumber()
+        i = 0
         for key in self.addresses_to.keys():
-            self.chores_assigned[key] = self.services[week_no % 3]
+            self.chores_assigned[key] = self.services[(week_no + i) % self.roomamtes]
+            i += 1
+            print(self.chores_assigned[key], key)
 
 
     def getDates(self):
