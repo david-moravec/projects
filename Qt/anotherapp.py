469021b0327571374593b57cf25b8c67bdcc1f2e
David Moravec <moravec@numsolution.cz>
2022-06-07 17:00:56 +0200

    Connect qlineedit with label.setText

--- Qt/anotherapp.py
+++ Qt/anotherapp.py
@@ -3,24 +3,13 @@ import sys
 from PyQt6.QtWidgets import (
     QApplication, 
     QMainWindow,
-    QPushButton,
+    QLabel,
+    QLineEdit,
+    QVBoxLayout,
+    QWidget,
 )
 
 from random import choice
-
-window_titles = [
-    "my App",
-    "my App",
-    "Still my app",
-    "Still my app",
-    "What On Earth",
-    "What On Earth",
-    "Fuck you",
-    "Fuck you",
-    "Suprise!",
-    "Suprise!",
-    "something got fucked up",
-]
 
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
@@ -30,26 +19,19 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("My App")
 
-        self.button = QPushButton("Press Me")
-        self.button.clicked.connect(self.button_clicked)
-        self.windowTitleChanged.connect(self.title_changed)
+        self.label = QLabel()
 
-        self.setCentralWidget(self.button)
+        self.input = QLineEdit()
+        self.input.textChanged.connect(self.label.setText)
 
+        layout = QVBoxLayout()
+        layout.addWidget(self.input)
+        layout.addWidget(self.label)
 
-    def button_clicked(self) -> None:
-        print(f'The button was clicked')
-        title: str = choice(window_titles)
-        print(f'setting title to {title}')
-        self.setWindowTitle(title)
+        container = QWidget()
+        container.setLayout(layout)
 
-
-    def title_changed(self) -> None:
-        print(f'Title changed to: {self.windowTitle()}')
-
-        if self.windowTitle() == window_titles[-1]:
-            self.button.setDisabled(True)
-
+        self.setCentralWidget(container)
 
 app = QApplication(sys.argv)
 
